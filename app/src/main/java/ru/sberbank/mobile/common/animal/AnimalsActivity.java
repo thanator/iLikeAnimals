fd4f1019d21b212fdc155e269497dfc00db7b3d9
QuickNickKoenig <niki.rabbit.1989@gmail.com>
2017-05-20 12:02:30 +0300

    Добавил адаптер для списка животных

--- app/src/main/java/ru/sberbank/mobile/common/animal/AnimalsActivity.java
+++ app/src/main/java/ru/sberbank/mobile/common/animal/AnimalsActivity.java
@@ -3,6 +3,8 @@ package ru.sberbank.mobile.common.animal;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 import ru.sberbank.backgroundtaskssample.R;
 
@@ -11,9 +13,15 @@ import ru.sberbank.backgroundtaskssample.R;
  */
 public class AnimalsActivity extends AppCompatActivity {
 
+    private AnimalsAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animals_activity);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        mAdapter = new AnimalsAdapter();
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/ru/sberbank/mobile/common/animal/AnimalsAdapter.java
+++ app/src/main/java/ru/sberbank/mobile/common/animal/AnimalsAdapter.java
@@ -1,9 +1,11 @@
 package ru.sberbank.mobile.common.animal;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,10 +40,51 @@ public class AnimalsAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = newView(parent);
+        }
+        bindView(itemView, position);
+        return itemView;
     }
 
-//    private View newView(ViewGroup parent) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
-//    }
+    public void setAnimals(List<Animal> animals) {
+        mAnimals.clear();
+        mAnimals.addAll(animals);
+        notifyDataSetChanged();
+    }
+
+    private View newView(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.animal_list_item, parent, false);
+        view.setTag(new ViewHolder(view));
+        return view;
+    }
+
+    private void bindView(View itemView, int position) {
+        ViewHolder holder = (ViewHolder) itemView.getTag();
+        Animal animal = mAnimals.get(position);
+        Context context = itemView.getContext();
+        holder.speciesTextView.setText(composeString(context, R.string.species, animal.getSpecies()));
+        holder.ageTextView.setText(composeString(context, R.string.age, String.valueOf(animal.getAge())));
+        holder.nameTextView.setText(composeString(context, R.string.name, animal.getName()));
+    }
+
+    private static String composeString(Context context, int propertyResId, String propertyValue) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(context.getString(propertyResId)).append(": ").append(propertyValue);
+        return sb.toString();
+    }
+
+    private static class ViewHolder {
+
+        public final TextView speciesTextView;
+        public final TextView ageTextView;
+        public final TextView nameTextView;
+
+        public ViewHolder(View itemView) {
+            speciesTextView = (TextView) itemView.findViewById(R.id.species_text_view);
+            ageTextView = (TextView) itemView.findViewById(R.id.age_text_view);
+            nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
+        }
+    }
 }
